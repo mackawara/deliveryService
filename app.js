bab4ea5977f58e4413c597553577ec745b707ea5
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-14 08:30:12 +0200

    changed server config

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 // server config
 const app = express();
-const PORT = process.env.PORT || 5007;
+const PORT = process.env.PORT || 5000;
 
 //app.use(dotenv);
 app.use(express.static("public"));
@@ -12,5 +12,6 @@ app.listen(PORT, () => {
   console.log("server listening");
 });
 app.get("/", (req, res) => {
+  console.log(__dirname + "public/deliveryhome.html");
   res.send(__dirname + "public/deliveryhome.html");
 });
