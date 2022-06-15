6b125f4d71a8c4184c7fdca31b972d246a8e4294
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-15 17:13:06 +0200

    added exact match regex

--- middleware/validation.js
+++ middleware/validation.js
@@ -16,7 +16,7 @@ const validationRules = () => {
       .isLength({ min: 2, max: 35 })
       .withMessage("Please ensure that your name is valid")
       // ignore is telling the code what to ignore
-      .isAlpha("en-GB", {"ignore":" -"} )
+      .isAlpha("en-GB", { ignore: " -" })
       .withMessage("Please ensure that there are no numerals in your name")
       .trim()
       .escape(),
@@ -26,7 +26,7 @@ const validationRules = () => {
       .withMessage("Please enter your name")
       .isLength({ min: 2, max: 35 })
       .withMessage("Please ensure that your name is valid")
-      .isAlpha("en-GB", {"ignore":" -"} )
+      .isAlpha("en-GB", { ignore: " -" })
       .withMessage("Please ensure that there are no numerals in your name")
       .trim()
       .escape(),
@@ -87,20 +87,22 @@ const validationRules = () => {
       .not()
       .matches(/SELECT ONE/g)
       .withMessage("please make a valid selection")
+      .matches(/\b(Empumalanga|Ingagula|Lwendulu| Ngumija|Makwika|cindrella|cbdHub|madumabisa)\b/i)
+      .withMessage("Please select from the given options")
       .notEmpty()
       .trim()
       .escape(),
   ];
 };
-const validate= (req, res, next) => {
-  console.log(req.body)
+const validate = (req, res, next) => {
+  console.log(req.body);
   const errors = validationResult(req);
   const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
     // Build your resulting errors however you want! String, object, whatever - it works!
     return `${param}: ${msg}`;
   };
   const result = errors.formatWith(errorFormatter);
-  console.log(errors)
+  console.log(errors);
 
   //const result = validationResult(req).formatWith(errorFormatter);
   if (!result.isEmpty()) {
@@ -109,6 +111,7 @@ const validate= (req, res, next) => {
     return res.json({ errors: result.mapped() });
   }
   console.log("Validation passed");
-    return next();}
+  return next();
+};
 module.exports = { validationRules, validate };
 // validation still needs to be consolidated
