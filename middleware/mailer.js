4e924b5d3b660085b65d855bc11d84b672b241ba
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-18 12:09:59 +0200

    add nodemailer for sendig email messages

---
+++ middleware/mailer.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+const mailer = async (req, res, next) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", //replace with your email provider
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+  // verify if nodemailer is authorized to send emails from that address.
+  transporter.verify(function (error, success, next) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("transorter has verified and is ready to send messages");
+    }
+  });
+
+  // this middleware configures the dat for saving to the DB
+
+  //input validation using Express validato
+
+  const sender = process.env.EMAIL;
+  const receiver = process.env.RECEIVER;
+  const mail = {
+    from: sender,
+    to: receiver,
+    subject: feedback.subject,
+    text: `From : ${feedback.name} \n Email: <${feedback.email}>  \n Mobile Number:${feedback.mobileNumber}  \n Feedback: ${feedback.text}`,
+  };
+
+  transporter.sendMail(mail, (err, res) => {
+    if (err) {
+      console.error(err);
+      // res.status(500).send("Something went wrong.");
+    } else {
+      res.status(200).send("Email successfully sent to recipient!");
+      console.log(`email has been sent succesfully to ${receiver}`);
+    }
+  });
+};
+module.exports = mailer;
