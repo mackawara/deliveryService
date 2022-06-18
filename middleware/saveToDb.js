7c60dcd795eaca4e94223e49ff00def71f9ff3e0
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-18 08:30:54 +0200

    changed  booking model

--- middleware/saveToDb.js
+++ middleware/saveToDb.js
@@ -66,29 +66,27 @@ const bookingModel = mongoose.model("booking", bookingSchema);
 
 let saveBookingToDataBase = async (req, res, next) => {
   console.log("attempting to save to database");
-  const body=req.body
+  const body = req.body;
   const senderName = req.body.senderName;
-  const senderNumber=body.senderNumber
+  const senderNumber = body.senderNumber;
   const typeOfParcel = req.body.typeOfParcel;
-  const pickUpSlot=body.pickUpSlot
-  const departureLocation=body.departureLocation
+  const pickUpSlot = body.pickUpSlot;
+  const departureLocation = body.departureLocation;
   const email = req.body.email;
-  const receiverName=body.receiverName
+  const receiverName = body.receiverName;
   const receiverNumber = req.body.receiverNumber;
-const destinationOfParcel=body.destinationOfParcel
+  const destinationOfParcel = body.destinationOfParcel;
 
   /* takes the valid */
   const booking = new bookingModel({
     senderName: senderName,
     senderNumber: senderNumber,
     typeOfParcel: typeOfParcel,
-   pickUpSlot:pickUpSlot,
-   typeOfParcel:typeOfParcel,
+    pickUpSlot: pickUpSlot,
     receiverNumber: receiverNumber,
-    receiverName:receiverName,
-    destinationOfParcel:destinationOfParcel,
-    departureLocation:departureLocation,
-
+    receiverName: receiverName,
+    destinationOfParcel: destinationOfParcel,
+    departureLocation: departureLocation,
     email: email,
   });
   /* query booking queries the database to see if this is not a duplicate of existing entry */
