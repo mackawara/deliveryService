7203cc22de29f640f1be325293f0954856e80adf
mackawara <mkawara@outlook.com>
2022-07-27 17:30:08 +0200

    added logic to send whatsapp messages

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const axios = require("axios").default;
 
 // server config
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 //app.use(dotenv);
 
@@ -25,61 +26,65 @@ const options = {
 const uri =
   process.env
     .MONGODB_URI; /* process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOLHQ_URI  */
-
+/*
 let connection = mongoose.connect(uri || process.env.DB_URI, options);
-
+ 
 const database = mongoose.connection;
 database.on("error", console.error.bind(console, "connection error:"));
 database.once("open", async function () {
   //wait for db to connect before running server
   console.log(`DAtabase connection established  and checking`);
 })
-  app.listen(PORT, () => {
-    console.log("server listening");
-  });
-  app.get("/", (req, res) => {
-    console.log(__dirname + "/public/deliveryhome.html");
-    res.sendFile(__dirname + "/public/deliveryhome.html");
-  });
+ */ app.listen(PORT, () => {
+  console.log("server listening");
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/deliveryhome.html");
+});
 
-  app.get("/home", (req, res) => {
-    console.log(__dirname + "/public/index.html");
-    res.sendFile(__dirname + "/public/index.html");
-  });
-  app.get("/signup", (req, res) => {
-    console.log(__dirname + "/public/signup.html");
-    res.sendFile(__dirname + "/public/signup.html");
-  });
-  app.use(express.static("public"));
-  app.use(express.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/home", (req, res) => {
+  console.log(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.get("/signup", (req, res) => {
+  console.log(__dirname + "/public/signup.html");
+  res.sendFile(__dirname + "/public/signup.html");
+});
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  // parse application/json
-  //validation
-  const { validationRules, validate } = require("./middleware/validation");
-  const saveToDb = require("./middleware/saveToDb");
-  const mailer = require("./middleware/mailer");
-  app.use(bodyParser.json());
+// parse application/json
+//validation
+const { validationRules, validate } = require("./middleware/validation");
+const saveToDb = require("./middleware/saveToDb");
+const mailer = require("./middleware/mailer");
+app.use(bodyParser.json());
 
-  //Contact Number for Whatsapp
-  const admin=process.env.ADMIN_NUMBER
-  const driver=process.env.DRIVER_NUMBER
-  app.post("/booking", validationRules(), validate, async(req, res) => {
-    const body=req.body
-    const wAmsg=`Delivery Booking alert \n
-     From sender : *${body.senderName}*, cell: ${body.senderNumber} \n
-     Pickup Location : *${body.departureLocation} *
-     to receiver :*${body.receiverName}*, cell: ${body.receiverNumber} \n
-     to Destination : ${body.destinationOfParcel} \n
-     Type of Parcel: *${body.typeOfParcel}* \n
-     Parcel should be picked up between : ${body.pickUpSLot}
-     `
-    
-      const response=await sendWatsp(wAmsg,`263775231426`)
-      res.send(response)
-  });
+//Contact Number for Whatsapp
+const admin = process.env.ADMIN_NUMBER;
+const driver = process.env.DRIVER_NUMBER;
+const token = process.env.TOKEN;
+const phoneID = process.env.PHONE_ID;
+const courage = process.env.COURAGE;
+app.post("/booking", validationRules(), validate, async (req, res) => {
+  const body = req.body;
+  const wAmsg = `*Delivery Booking alert* \n
+From sender : *${body.senderName}*, cell: ${body.senderNumber} \n
+Pickup Location : *${body.departureLocation} * \n
+To receiver :*${body.receiverName}*, cell: ${body.receiverNumber} \n
+To Destination : ${body.destinationOfParcel} \n
+Type of Parcel: *${body.parcel}* \n
+Parcel should be picked up between : ${body.pickUpSlot}
+     `;
+  console.log(wAmsg);
+  sendWatsp(wAmsg, courage);
+  //console.log(response);
+  res.send(JSON.stringify({ Booking: wAmsg }));
+});
 
-const sendWatsp = async (booking,number) => {
+const sendWatsp = async (booking, number) => {
+  console.log(booking, number, token, phoneID);
   axios({
     method: "POST", // Required, HTTP method, a string, e.g. POST, GET
     url:
@@ -95,12 +100,10 @@ const sendWatsp = async (booking,number) => {
     headers: { "Content-Type": "application/json" },
   })
     .then((data) => {
-      console.log(data.headers);
-      return ("Booking was saved , confirmation was also sent to your email");
+      console.log("success");
+      return "Booking was saved , confirmation was also sent to your email";
     })
     .catch((err) => {
-     return `There was an error on the server please try again `
+      return `There was an error on the server please try again `;
     });
 };
-
-
